bcf24a6fd305480843b923de51996af8cec38681
maitreebain <maitreebain@gmail.com>
2020-02-06 10:49:19 -0500

    Initial Commit

---
+++ NYT-TopStories/TopStoriesTabController.swift
@@ -0,0 +1,19 @@
+//
+//  TopStoriesTabController.swift
+//  NYT-TopStories
+//
+//  Created by Maitree Bain on 2/6/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+
+}
